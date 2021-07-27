84bab23a136a71e101a8de2349226d097bb6d31e
totos92barberio <68277798+totos92barberio@users.noreply.github.com>
2021-07-27 11:42:13 +0200

    nuova classe di config

--- src/main/java/it/pagopa/pn/commons/cassandra/config/CassandraConfig.java
+++ src/main/java/it/pagopa/pn/commons/cassandra/config/CassandraConfig.java
@@ -4,9 +4,23 @@ import com.datastax.oss.driver.api.core.CqlSession;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.SessionFactory;
+import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
+import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.config.SessionFactoryFactoryBean;
+import org.springframework.data.cassandra.core.CassandraOperations;
+import org.springframework.data.cassandra.core.CassandraTemplate;
+import org.springframework.data.cassandra.core.convert.CassandraConverter;
+import org.springframework.data.cassandra.core.convert.MappingCassandraConverter;
+import org.springframework.data.cassandra.core.mapping.CassandraMappingContext;
+import org.springframework.data.cassandra.core.mapping.SimpleUserTypeResolver;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
 
 @Configuration
+// @EnableCassandraRepositories(basePackages = { "org.springframework.data.cassandra.example" })
 public class CassandraConfig {
+
     // TODO indagare effettiva necessità di questa classe
 
     private final String keyspace;
@@ -23,7 +37,46 @@ public class CassandraConfig {
     }
 
     @Bean
-    public CqlSession getSession(){
-        return CqlSession.builder().withKeyspace(keyspace).withAuthCredentials(username,password).build();
+    public CqlSessionFactoryBean session() {
+
+        CqlSessionFactoryBean session = new CqlSessionFactoryBean();
+        session.setLocalDatacenter("");
+        session.setKeyspaceName(keyspace);
+        session.setPassword(password);
+        session.setUsername(username);
+        session.setLocalDatacenter("datacenter1"
+        );
+
+        return session;
+    }
+
+    @Bean
+    public SessionFactoryFactoryBean sessionFactory(CqlSession session, CassandraConverter converter) {
+
+        SessionFactoryFactoryBean sessionFactory = new SessionFactoryFactoryBean();
+        sessionFactory.setSession(session);
+        sessionFactory.setConverter(converter);
+        sessionFactory.setSchemaAction(SchemaAction.NONE);
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public CassandraMappingContext mappingContext(CqlSession cqlSession) {
+
+        CassandraMappingContext mappingContext = new CassandraMappingContext();
+        mappingContext.setUserTypeResolver(new SimpleUserTypeResolver(cqlSession));
+
+        return mappingContext;
+    }
+
+    @Bean
+    public CassandraConverter converter(CassandraMappingContext mappingContext) {
+        return new MappingCassandraConverter(mappingContext);
+    }
+
+    @Bean
+    public CassandraOperations cassandraTemplate(SessionFactory sessionFactory, CassandraConverter converter) {
+        return new CassandraTemplate(sessionFactory, converter);
     }
 }
