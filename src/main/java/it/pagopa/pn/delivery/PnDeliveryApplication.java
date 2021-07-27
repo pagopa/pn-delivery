2cef68e982f6f7d923b7fb42cf5add87aa89b9b8
totos92barberio <68277798+totos92barberio@users.noreply.github.com>
2021-07-27 10:55:21 +0200

    package scan del main

--- src/main/java/it/pagopa/pn/delivery/PnDeliveryApplication.java
+++ src/main/java/it/pagopa/pn/delivery/PnDeliveryApplication.java
@@ -3,7 +3,7 @@ package it.pagopa.pn.delivery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"it.pagopa.pn.commons.cassandra"})
+@SpringBootApplication(scanBasePackages = {"it.pagopa.pn.commons"})
 public class PnDeliveryApplication {
 
 	public static void main(String[] args) {
