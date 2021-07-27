bc2ab5d175bdf853e1a409157e4e0516068324d9
totos92barberio <68277798+totos92barberio@users.noreply.github.com>
2021-07-27 17:43:19 +0200

    config corrette

--- src/main/java/it/pagopa/pn/commons/cassandra/config/CassandraConfig.java
+++ src/main/java/it/pagopa/pn/commons/cassandra/config/CassandraConfig.java
@@ -1,14 +1,9 @@
 package it.pagopa.pn.commons.cassandra.config;
 
 
-import com.datastax.oss.driver.api.core.CqlSession;
-import com.datastax.oss.driver.api.core.CqlSessionBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.env.Environment;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
 import org.springframework.data.cassandra.config.SchemaAction;
@@ -17,21 +12,18 @@ import org.springframework.data.cassandra.core.cql.keyspace.DataCenterReplicatio
 import org.springframework.data.cassandra.core.cql.keyspace.DropKeyspaceSpecification;
 import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
-import org.springframework.core.env.Environment;
 
-import java.net.InetSocketAddress;
 import java.util.Arrays;
 import java.util.List;
 
 
 @Configuration
-@EnableCassandraRepositories(basePackages="it.pagopa.pn.delivery.model")
+@EnableCassandraRepositories(basePackages="it.pagopa.pn.delivery.model.notification")
 public class CassandraConfig extends AbstractCassandraConfiguration {
 
     private final String keyspace;
     private final String username;
     private final String password;
-
 
     CassandraConfig(
             @Value("${spring.data.cassandra.keyspace-name}") String keyspace,
@@ -58,15 +50,14 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
         CreateKeyspaceSpecification specification = CreateKeyspaceSpecification
                 .createKeyspace(keyspace)
                 .ifNotExists()
-                .with(KeyspaceOption.DURABLE_WRITES, true)
-                .withNetworkReplication(DataCenterReplication.of("foo", 1), DataCenterReplication.of("bar", 2));
+                .with(KeyspaceOption.DURABLE_WRITES, true);
 
         return Arrays.asList(specification);
     }
 
     @Override
     protected List<DropKeyspaceSpecification> getKeyspaceDrops() {
-        return Arrays.asList(DropKeyspaceSpecification.dropKeyspace(keyspace));
+        return Arrays.asList(DropKeyspaceSpecification.dropKeyspace(keyspace).ifExists());
     }
 
     @Bean
@@ -80,7 +71,7 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
 
     @Override
     public String[] getEntityBasePackages() {
-        return new String[] { "it.pagopa.pn.delivery.model" };
+        return new String[] { "it.pagopa.pn.delivery.model.notification" };
     }
 
 }
