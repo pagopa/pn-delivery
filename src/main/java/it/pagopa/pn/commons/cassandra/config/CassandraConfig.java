33210c0cb83029ca6c847fc9737696032b87c004
totos92barberio <68277798+totos92barberio@users.noreply.github.com>
2021-07-30 14:30:09 +0200

    property bean

--- src/main/java/it/pagopa/pn/commons/cassandra/config/CassandraConfig.java
+++ src/main/java/it/pagopa/pn/commons/cassandra/config/CassandraConfig.java
@@ -1,6 +1,7 @@
 package it.pagopa.pn.commons.cassandra.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +9,6 @@ import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
 import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
-import org.springframework.data.cassandra.core.cql.keyspace.DataCenterReplication;
-import org.springframework.data.cassandra.core.cql.keyspace.DropKeyspaceSpecification;
 import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
@@ -21,22 +20,18 @@ import java.util.List;
 @EnableCassandraRepositories(basePackages="it.pagopa.pn.delivery.model.notification")
 public class CassandraConfig extends AbstractCassandraConfiguration {
 
-    private final String keyspace;
-    private final String username;
-    private final String password;
+    private final CassandraProperties cassandraProperties;
 
+    @Autowired
     CassandraConfig(
-            @Value("${spring.data.cassandra.keyspace-name}") String keyspace,
-            @Value("${spring.data.cassandra.username}") String username,
-            @Value("${spring.data.cassandra.password}") String password) {
-        this.keyspace = keyspace;
-        this.username = username;
-        this.password = password;
+            CassandraProperties cassandraProperties) {
+
+        this.cassandraProperties = cassandraProperties;
     }
 
     @Override
     protected String getKeyspaceName() {
-        return keyspace;
+        return cassandraProperties.getKeyspaceName();
     }
 
     @Override
@@ -48,7 +43,7 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
     protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
 
         CreateKeyspaceSpecification specification = CreateKeyspaceSpecification
-                .createKeyspace(keyspace)
+                .createKeyspace(cassandraProperties.getKeyspaceName())
                 .ifNotExists()
                 .with(KeyspaceOption.DURABLE_WRITES, true);
 
@@ -59,14 +54,14 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
     @Override
     public CqlSessionFactoryBean cassandraSession() {
         CqlSessionFactoryBean cassandraSession = super.cassandraSession();//super session should be called only once
-        cassandraSession.setUsername(username);
-        cassandraSession.setPassword(password);
+        cassandraSession.setUsername(cassandraProperties.getUsername());
+        cassandraSession.setPassword(cassandraProperties.getPassword());
         return cassandraSession;
     }
 
     @Override
     public String[] getEntityBasePackages() {
-        return new String[] { "it.pagopa.pn.delivery.model.notification" };
+        return new String[] {cassandraProperties.getEntitypackage()};
     }
 
 }
--- src/main/java/it/pagopa/pn/delivery/PnDeliveryApplication.java
+++ src/main/java/it/pagopa/pn/delivery/PnDeliveryApplication.java
@@ -1,12 +1,15 @@
 package it.pagopa.pn.delivery;
 
+import it.pagopa.pn.commons.cassandra.config.CassandraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.reactive.result.view.RedirectView;
 
+@EnableConfigurationProperties(CassandraProperties.class)
 @SpringBootApplication(scanBasePackages = {"it.pagopa.pn.commons", "it.pagopa.pn.delivery"})
 public class PnDeliveryApplication {
 
