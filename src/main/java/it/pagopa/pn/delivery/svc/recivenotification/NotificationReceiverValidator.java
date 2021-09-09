de7758af5b5b40a07b0f290d72f6c6dded757f06
Simone <SimoneLubrano94@gmail.com>
2021-09-09 12:19:45 +0200

    Added validation on attachments of Notification

--- src/main/java/it/pagopa/pn/delivery/svc/recivenotification/NotificationReceiverValidator.java
+++ src/main/java/it/pagopa/pn/delivery/svc/recivenotification/NotificationReceiverValidator.java
@@ -1,11 +1,15 @@
 package it.pagopa.pn.delivery.svc.recivenotification;
 
 import it.pagopa.pn.api.dto.notification.*;
+import it.pagopa.pn.commons.exceptions.PnEncodingException;
 import it.pagopa.pn.commons.exceptions.PnValidationException;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.stereotype.Component;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
+import java.math.BigInteger;
 import java.util.Set;
 
 @Component
@@ -28,4 +32,46 @@ public class NotificationReceiverValidator {
         return validator.validate( notification, NotificationJsonViews.New.class );
     }
 
+    public boolean checkNotificationAttachmentsBodyIsBase64(Notification notification){
+        for(NotificationAttachment attachment : notification.getDocuments()){
+            if(!Base64.isBase64(attachment.getBody())) {
+                throw new PnEncodingException();
+            }
+        }
+        return true;
+    }
+
+    public boolean checkNotificationAttachmentsDigestIsSha256(Notification notification){
+        String sha256Str;
+        for(NotificationAttachment attachment : notification.getDocuments()){
+            if (Base64.isBase64(attachment.getBody())) {
+                byte[] base64Decoded = Base64.decodeBase64(attachment.getBody());
+                sha256Str = DigestUtils.sha256Hex(base64Decoded);
+            }
+            else
+                throw new PnEncodingException();
+
+            if(!attachment.getDigests().getSha256().equals(sha256Str))
+                throw new PnEncodingException();
+        }
+        return true;
+    }
+
+    private boolean checkF24AttachmentsAreBase64(Notification notification){
+
+        NotificationAttachment flatRateAttachment = notification.getPayment().getF24().getFlatRate();
+        NotificationAttachment digitalAttachment = notification.getPayment().getF24().getDigital();
+        NotificationAttachment analogAttachment = notification.getPayment().getF24().getAnalog();
+
+        if(!Base64.isBase64(flatRateAttachment.getBody()))
+            throw new PnEncodingException();
+
+        if(!Base64.isBase64(digitalAttachment.getBody()))
+            throw new PnEncodingException();
+
+        if(!Base64.isBase64(analogAttachment.getBody()))
+            throw new PnEncodingException();
+
+        return true;
+    }
 }
