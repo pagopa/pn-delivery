881ede7c8daef1305afcf5c12562909a0f9873e0
alfonsogiustini <a.giustini@sirti.it>
2021-09-17 12:45:56 +0200

    pulizia codice

--- src/main/java/it/pagopa/pn/delivery/middleware/newnotificationproducer/SqsNotificationViewedProducer.java
+++ src/main/java/it/pagopa/pn/delivery/middleware/newnotificationproducer/SqsNotificationViewedProducer.java
@@ -1,16 +1,15 @@
 package it.pagopa.pn.delivery.middleware.newnotificationproducer;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
-import it.pagopa.pn.api.dto.events.PnDeliveryNewNotificationEvent;
+
 import it.pagopa.pn.api.dto.events.PnDeliveryNotificationViewedEvent;
 import it.pagopa.pn.commons.abstractions.impl.AbstractSqsMomProducer;
 import it.pagopa.pn.commons.abstractions.impl.MiddlewareTypes;
 import it.pagopa.pn.delivery.PnDeliveryConfigs;
-import it.pagopa.pn.delivery.middleware.NewNotificationProducer;
 import it.pagopa.pn.delivery.middleware.NotificationViewedProducer;
-
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.stereotype.Component;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Component
@@ -18,7 +17,7 @@ import software.amazon.awssdk.services.sqs.SqsClient;
 public class SqsNotificationViewedProducer extends AbstractSqsMomProducer<PnDeliveryNotificationViewedEvent> implements NotificationViewedProducer {
 
     public SqsNotificationViewedProducer(SqsClient sqsClient, ObjectMapper objectMapper, PnDeliveryConfigs cfg ) {
-        super(sqsClient, cfg.getTopics().getNotificationAcknowledgement(), objectMapper, PnDeliveryNotificationViewedEvent.class);
+        super(sqsClient, cfg.getTopics().getNewNotifications(), objectMapper, PnDeliveryNotificationViewedEvent.class);
     }
 
 }
