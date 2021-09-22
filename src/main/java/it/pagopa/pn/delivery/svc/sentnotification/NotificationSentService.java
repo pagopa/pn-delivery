d1ec0bed11362be3d08ce5ec4bed3203366ddfb2
marco-vit-pagopa <marco.vit@pagopa.it>
2021-09-22 06:52:47 +0200

    PN-279 aggiunta informazioni di timeline e stato

--- src/main/java/it/pagopa/pn/delivery/svc/sentnotification/NotificationSentService.java
+++ src/main/java/it/pagopa/pn/delivery/svc/sentnotification/NotificationSentService.java
@@ -1,8 +1,17 @@
 package it.pagopa.pn.delivery.svc.sentnotification;
 
 import java.time.Clock;
+import java.time.Instant;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import it.pagopa.pn.api.dto.notification.status.NotificationStatusHistoryElement;
+import it.pagopa.pn.api.dto.notification.timeline.TimelineElement;
+import it.pagopa.pn.commons_delivery.middleware.TimelineDao;
+import it.pagopa.pn.commons_delivery.utils.StatusUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,15 +30,23 @@ public class NotificationSentService {
 	private final Clock clock;
 	private final NotificationViewedProducer notificationAcknowledgementProducer;
 	private final NotificationDao notificationDao;
+	private final TimelineDao timelineDao;
+	private final StatusUtils statusUtils;
 
 	@Autowired
-	public NotificationSentService( Clock clock,
-			AttachmentService attachmentService,
-			NotificationViewedProducer notificationAcknowledgementProducer, NotificationDao notificationDao ) {
+	public NotificationSentService(Clock clock,
+								   AttachmentService attachmentService,
+								   NotificationViewedProducer notificationAcknowledgementProducer,
+								   NotificationDao notificationDao,
+								   TimelineDao timelineDao,
+								   StatusUtils statusUtils
+	) {
 		this.clock = clock;
 		this.attachmentService = attachmentService;
 		this.notificationAcknowledgementProducer = notificationAcknowledgementProducer;
 		this.notificationDao = notificationDao;
+		this.timelineDao = timelineDao;
+		this.statusUtils = statusUtils;
 	}
 
 	/**
@@ -41,20 +58,30 @@ public class NotificationSentService {
 	 * 
 	 */
 	public Notification getSentNotification(String iun) {
-		log.debug( "Received Notification START {} " , iun);
-		Notification notification = null;
-		
-		log.info( "Retrieve notification with iun={} ", iun );
-        Optional<Notification> optNotification = notificationDao.getNotificationByIun( iun );
+		Optional<Notification> optNotification = notificationDao.getNotificationByIun( iun );
         
         if (optNotification.isPresent() ) {
-        	notification = optNotification.get();
+			Set<TimelineElement> rawTimeline = timelineDao.getTimeline(iun);
+			List<TimelineElement> timeline = rawTimeline
+					.stream()
+					.sorted( Comparator.comparing( TimelineElement::getTimestamp ))
+					.collect(Collectors.toList());
+
+			int numberOfRecipients = optNotification.get().getRecipients().size();
+			Instant createdAt =  optNotification.get().getSentAt();
+			List<NotificationStatusHistoryElement>  statusHistory = statusUtils
+					             .getStatusHistory( rawTimeline, numberOfRecipients, createdAt );
+
+			return optNotification.get()
+					.toBuilder()
+					.timeline( timeline )
+					.notificationStatusHistory( statusHistory )
+					.notificationStatus( statusUtils.getCurrentStatus( statusHistory ))
+					.build();
         } else {
         	log.debug( "Error in retrieving Notification with iun {}", iun );
 			throw new PnInternalException( "Error in retrieving Notification with iun " + iun );
         }
-        
-		return notification;
 	}
 
 }
