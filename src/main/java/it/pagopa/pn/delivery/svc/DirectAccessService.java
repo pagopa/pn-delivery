53d95d7276252146224883afb833ec3d6da09fef
Simone Rondinella <simone.rondinella@pagopa.it>
2022-04-06 18:03:36 +0200

    Eliminazione Entity non necessaria, logica spostata su service

--- src/main/java/it/pagopa/pn/delivery/svc/DirectAccessService.java
+++ src/main/java/it/pagopa/pn/delivery/svc/DirectAccessService.java
@@ -1,31 +1,36 @@
 package it.pagopa.pn.delivery.svc;
 
+import it.pagopa.pn.api.dto.notification.Notification;
+import it.pagopa.pn.api.dto.notification.NotificationRecipient;
 import it.pagopa.pn.api.dto.notification.directaccesstoken.DirectAccessToken;
 import it.pagopa.pn.commons.pnclients.recipientschallenge.RecipientsChallenge;
-import it.pagopa.pn.delivery.middleware.directaccesstokendao.DirectAccessTokenDao;
+import it.pagopa.pn.delivery.middleware.NotificationDao;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @Service
 public class DirectAccessService {
 
-	private final DirectAccessTokenDao directAccessTokenDao;
+	private final NotificationDao notificationDao;
 	private final RecipientsChallenge recipientsChallenge;
 
-	@Autowired
-	public DirectAccessService(DirectAccessTokenDao directAccessTokenDao,
-							   RecipientsChallenge recipientsChallenge) {
-		this.directAccessTokenDao = directAccessTokenDao;
+	public DirectAccessService(NotificationDao notificationDao, RecipientsChallenge recipientsChallenge) {
+		this.notificationDao = notificationDao;
 		this.recipientsChallenge = recipientsChallenge;
 	}
-
-	public Optional<DirectAccessToken> doChallenge( String directAccessToken, String userSecret ) {
+	
+	public String generateToken(String iun, String taxId) {
+		return iun + "_" + taxId + "_" + UUID.randomUUID();
+	}
+	
+	public Optional<DirectAccessToken> doChallenge(String directAccessToken, String userSecret ) {
 		log.info( "Retrieve direct access token info for token={}", directAccessToken );
-		Optional<DirectAccessToken> directAccessTokenInfo = directAccessTokenDao.getDirectAccessToken( directAccessToken );
+		Optional<DirectAccessToken> directAccessTokenInfo = checkAndGetDirectAccessToken(directAccessToken);
 		if (directAccessTokenInfo.isPresent()) {
 			log.debug( "Retrieve secret START" );
 			String secret = recipientsChallenge.getSecret( directAccessTokenInfo.get().getTaxId() );
@@ -39,8 +44,55 @@ public class DirectAccessService {
 		}
 		return directAccessTokenInfo;
 	}
+	
+	private Optional<DirectAccessToken> checkAndGetDirectAccessToken(String token) {
+		String iun = getIunFromToken(token);
+		
+		if(iun != null){
+			log.debug( "Get direct access token is for iun {}",iun );
 
+			Optional<Notification> notificationOtp = notificationDao.getNotificationByIun(iun);
+			if(notificationOtp.isPresent()){
+				log.debug( "Notification is present for iun {}",iun );
+				return getDirectAccessToken(token, iun, notificationOtp.get());
+			}else{
+				return handleInvalidToken(token, iun);
+			}
+		}else {
+			return handleInvalidToken(token, null);
+		}
+	}
 
+	private String getIunFromToken(String token) {
+		return (token != null && token.contains("_")) ? token.substring(0, token.indexOf("_")) : null;
+	}
 
+	@NotNull
+	private Optional<DirectAccessToken> getDirectAccessToken(String token, String iun, Notification notification) {
+
+		Optional<String> taxIdOtp =
+				notification.getRecipients().stream()
+						.filter(recipient -> token.equals(recipient.getToken()))
+						.map(NotificationRecipient::getTaxId).findFirst();
+
+		if (taxIdOtp.isPresent()){
+			log.debug( "TaxId is present for iun {}",iun );
+
+			DirectAccessToken directAccessToken = DirectAccessToken.builder()
+					.token(token)
+					.iun(iun)
+					.taxId(taxIdOtp.get())
+					.build();
+			
+			return Optional.of(directAccessToken);
+		}else {
+			return handleInvalidToken(token, iun);
+		}
+	}
+
+	private Optional<DirectAccessToken> handleInvalidToken(String token, String iun) {
+		log.warn("Token {} is not valid for iun {}", token, iun);
+		return Optional.empty();
+	}
 
 }
