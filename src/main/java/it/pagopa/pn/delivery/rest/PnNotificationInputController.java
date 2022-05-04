363bb0a12c4b91a9b2a12010c35ab5760424ed3a
Alessandro Masci <alessandro.masci@pagopa.it>
2022-05-04 11:12:59 +0200

    PN-1150: fix search

--- src/main/java/it/pagopa/pn/delivery/rest/PnNotificationInputController.java
+++ src/main/java/it/pagopa/pn/delivery/rest/PnNotificationInputController.java
@@ -1,30 +1,20 @@
 package it.pagopa.pn.delivery.rest;
 
-import it.pagopa.pn.api.dto.events.ServiceLevelType;
 import it.pagopa.pn.api.dto.notification.Notification;
-import it.pagopa.pn.api.dto.notification.NotificationJsonViews;
-import it.pagopa.pn.api.dto.notification.NotificationSender;
-import it.pagopa.pn.api.dto.preload.PreloadRequest;
-import it.pagopa.pn.api.dto.preload.PreloadResponse;
-import it.pagopa.pn.api.rest.PnDeliveryRestApi_methodReceiveNotification;
-import it.pagopa.pn.api.rest.PnDeliveryRestConstants;
 import it.pagopa.pn.commons.exceptions.PnValidationException;
 import it.pagopa.pn.delivery.PnDeliveryConfigs;
 import it.pagopa.pn.delivery.generated.openapi.server.v1.api.NewNotificationApi;
 import it.pagopa.pn.delivery.generated.openapi.server.v1.dto.*;
-import it.pagopa.pn.delivery.rest.dto.ConstraintViolationImpl;
 import it.pagopa.pn.delivery.rest.dto.ResErrorDto;
 import it.pagopa.pn.delivery.rest.utils.HandleValidation;
+import it.pagopa.pn.delivery.svc.NotificationReceiverService;
 import it.pagopa.pn.delivery.svc.S3PresignedUrlService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.annotation.JsonView;
-
-import it.pagopa.pn.delivery.svc.NotificationReceiverService;
-
-import java.util.*;
+import java.util.List;
 
 @Slf4j
 @RestController
