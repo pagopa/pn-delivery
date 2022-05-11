1d15e3cb3e4cf6cd737cb16b891a0b20d69712c0
MTurraPpa <94441477+MTurraPpa@users.noreply.github.com>
2022-05-11 09:08:22 +0200

    PN-725: fixed small issues

--- src/test/load-test/entrypoint.sh
+++ src/test/load-test/entrypoint.sh
@@ -1,3 +1,4 @@
+export JVM_ARGS="-Dnashorn.args=--no-deprecation-warning"
 
 echo ""
 echo "#### Launch tests with:"
@@ -10,10 +11,9 @@ bzt -l /tmp/artifacts/bzt.log "$@" /test/main.yaml
 
 
 echo "#### Compute data summary"
-# ( cd /tmp/artifacts && python3 /summary.py )
-
-echo "#### Copy outputs"
+( cd /tmp/artifacts && python3 /summary.py )
 timestamp=$(date +%Y%m%d-%H%M%S)
+echo "#### Copy outputs with timestamp=$timestamp"
 cp /tmp/artifacts/error.jtl /minimal_outputs/error-${timestamp}.jtl
 cp /tmp/artifacts/kpi.jtl   /minimal_outputs/kpi-${timestamp}.jtl
 cp /tmp/artifacts/summary.json   /minimal_outputs/summary-${timestamp}.json
