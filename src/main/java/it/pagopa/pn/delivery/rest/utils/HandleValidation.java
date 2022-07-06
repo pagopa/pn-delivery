4425fce453d7754aa0f3884934b5a33bb622098b
MTurraPpa <94441477+MTurraPpa@users.noreply.github.com>
2022-07-06 10:40:21 +0200

    PN-1703: Handle api validation errors.

--- src/main/java/it/pagopa/pn/delivery/rest/utils/HandleValidation.java
+++ src/main/java/it/pagopa/pn/delivery/rest/utils/HandleValidation.java
@@ -5,12 +5,33 @@ import it.pagopa.pn.delivery.rest.dto.ErrorDto;
 import it.pagopa.pn.delivery.rest.dto.ResErrorDto;
 import org.springframework.http.ResponseEntity;
 
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class HandleValidation {
     private HandleValidation(){}
-    
+    public static ResponseEntity<ResErrorDto> handleValidationException(ConstraintViolationException ex, String statusError){
+        List<ErrorDto> listErrorDto = ex.getConstraintViolations().stream()
+                .map(msg ->
+                        ErrorDto.builder()
+                                .message(msg.getMessage())
+                                .property(msg.getPropertyPath()!= null ? msg.getPropertyPath().toString() : "")
+                                .code(msg.getConstraintDescriptor()!= null ? msg.getConstraintDescriptor()
+                                        .getAnnotation()
+                                        .getClass()
+                                        .getSimpleName() : "")
+                                .build()
+                )
+                .collect(Collectors.toList());
+
+        return ResponseEntity.badRequest()
+                .body(ResErrorDto.builder()
+                        .status(statusError)
+                        .errorDtoList(listErrorDto)
+                        .build());
+    }
+
     public static ResponseEntity<ResErrorDto> handleValidationException(PnValidationException ex, String statusError){
         List<ErrorDto> listErrorDto = ex.getValidationErrors().stream()
                 .map(msg ->
