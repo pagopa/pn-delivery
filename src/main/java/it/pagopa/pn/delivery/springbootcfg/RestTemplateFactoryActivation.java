06c29d70bccc49f58bef351c9cd00aeaa64acace
Simone Rondinella <simone.rondinella@pagopa.it>
2022-07-28 11:04:59 +0200

    PN-1905 Gestione rest template portato in commons

--- src/main/java/it/pagopa/pn/delivery/springbootcfg/RestTemplateFactoryActivation.java
+++ src/main/java/it/pagopa/pn/delivery/springbootcfg/RestTemplateFactoryActivation.java
@@ -1,23 +1,8 @@
 package it.pagopa.pn.delivery.springbootcfg;
 
 import it.pagopa.pn.commons.pnclients.RestTemplateFactory;
-import it.pagopa.pn.commons.pnclients.RestTemplateResponseErrorHandler;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateFactoryActivation extends RestTemplateFactory {
-    
-    @Bean
-    @Qualifier("withTracing")
-    @Primary
-    @Override
-    public RestTemplate restTemplateWithTracing() {
-        RestTemplate restTemplate =  super.restTemplateWithTracing();
-        restTemplate.setErrorHandler(new RestTemplateResponseErrorHandler());
-        return restTemplate;
-    }
 }
