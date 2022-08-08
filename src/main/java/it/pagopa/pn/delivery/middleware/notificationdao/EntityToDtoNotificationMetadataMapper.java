ca0c956b65badfa47a6a1549f9924a1eae576b34
Simone Rondinella <simone.rondinella@pagopa.it>
2022-08-08 14:15:57 +0200

    PN-2020 aggiunto controllo sul tableRow diverso da null

--- src/main/java/it/pagopa/pn/delivery/middleware/notificationdao/EntityToDtoNotificationMetadataMapper.java
+++ src/main/java/it/pagopa/pn/delivery/middleware/notificationdao/EntityToDtoNotificationMetadataMapper.java
@@ -14,6 +14,12 @@ import java.time.ZoneOffset;
 public class EntityToDtoNotificationMetadataMapper {
 
     public NotificationSearchRow entity2Dto(NotificationMetadataEntity entity) {
+        OffsetDateTime requestAcceptedAt = null;
+        
+        if (entity.getTableRow() != null){
+            requestAcceptedAt = OffsetDateTime.parse( entity.getTableRow().get( "acceptedAt" ) );
+        }
+        
         return NotificationSearchRow.builder()
                 .iun( entity.getIun_recipientId().substring(0 ,entity.getIun_recipientId().indexOf("##")) )
                 .sender( entity.getTableRow().get( "senderDenomination" ) )
@@ -21,7 +27,7 @@ public class EntityToDtoNotificationMetadataMapper {
                 .sentAt( entity.getSentAt().atOffset( ZoneOffset.UTC ))
                 .subject( entity.getTableRow().get( "subject" ) )
                 .paProtocolNumber( entity.getTableRow().get("paProtocolNumber") )
-                .requestAcceptedAt(OffsetDateTime.parse( entity.getTableRow().get( "acceptedAt" ) ) )
+                .requestAcceptedAt( requestAcceptedAt )
                 //.group( entity.getNotificationGroup() )
                 .notificationStatus( NotificationStatus.valueOf( entity.getNotificationStatus() ))
                 .build();
