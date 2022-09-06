803daa4e0dc135867b8f61d179cb45e7bc4edc1a
MTurraPpa <94441477+MTurraPpa@users.noreply.github.com>
2022-09-06 09:21:12 +0200

    OpenAPI external per destinatario

--- scripts/openapi/internal-to-external.sh
+++ scripts/openapi/internal-to-external.sh
@@ -3,3 +3,8 @@ cat docs/openapi/api-internal-b2b-pa-v1.yaml \
     | grep -v "# NO EXTERNAL" \
     | sed -e '/# ONLY EXTERNAL/s/^#//' \
     > docs/openapi/api-external-b2b-pa-v1.yaml
+
+cat docs/openapi/api-internal-web-recipient-v1.yaml \
+    | grep -v "# NO EXTERNAL" \
+    | sed -e '/# ONLY EXTERNAL/s/^#//' \
+    > docs/openapi/api-external-web-recipient-v1.yaml
