aa571280894cbc92fab6ff93890d398fce637904
Alessandro Masci <alessandro.masci@pagopa.it>
2022-09-23 11:08:53 +0200

    PN-2071: add urlEncoding token

--- src/main/java/it/pagopa/pn/delivery/svc/NotificationReceiverService.java
+++ src/main/java/it/pagopa/pn/delivery/svc/NotificationReceiverService.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Base64Utils;
 
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.Clock;
 import java.time.Instant;
@@ -130,8 +131,9 @@ public class NotificationReceiverService {
 	}
 
 	public String generateToken(String iun, String taxId) {
-		return Base64Utils.encodeToString( (iun + "_" + taxId + "_" + UUID.randomUUID()).getBytes(StandardCharsets.UTF_8) );
+		byte[] bytes = (iun + "_" + taxId + "_" + UUID.randomUUID()).getBytes(StandardCharsets.UTF_8);
+		String base64Encoded = Base64Utils.encodeToString(bytes);
+		return URLEncoder.encode(base64Encoded, StandardCharsets.UTF_8);
 	}
-
 
 }
