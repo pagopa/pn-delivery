81f901a3734c228cc7539b2d74207da1f294c02d
fabriziolana <fabrizio.lana@necsy.it>
2022-10-13 17:39:34 +0200

    PN-2055: modifiche post review

--- scripts/aws/lambda/extract-lambda-in-single-file-js.sh
+++ scripts/aws/lambda/extract-lambda-in-single-file-js.sh
@@ -5,6 +5,6 @@ rm -f index.js
 rm -f lambda.zip
 
 # estraggo il contenuto della lambda dal microservice.yml
-sed -n -e "/ZipFile: |/,/#ENDpndeliveryinserttrigger/{ /ZipFile: |/d; /#ENDpndeliveryinserttrigger/d; p; }" < ../cfn/storage.yml | tail -n +1 > index.js
+sed -n -e "/ZipFile: |/,/#ENDpndeliveryinserttrigger/{ /ZipFile: |/d; /#ENDpndeliveryinserttrigger/d; p; }" < ../cfn/microservice.yml | tail -n +1 > index.js
 
 
