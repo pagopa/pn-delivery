04bc9edc0e7a682e9b11e45f43cd1a2e61bea9eb
fabriziolana <fabrizio.lana@necsy.it>
2022-10-18 18:09:10 +0200

    PN-2427: fix mancanza messagegroupid per cambio coda tipo fifo

--- src/main/java/it/pagopa/pn/delivery/middleware/newnotificationproducer/SqsNotificationViewedProducer.java
+++ src/main/java/it/pagopa/pn/delivery/middleware/newnotificationproducer/SqsNotificationViewedProducer.java
@@ -1,15 +1,15 @@
 package it.pagopa.pn.delivery.middleware.newnotificationproducer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import it.pagopa.pn.api.dto.events.AbstractSqsFifoMomProducer;
 import it.pagopa.pn.api.dto.events.PnDeliveryNotificationViewedEvent;
-import it.pagopa.pn.commons.abstractions.impl.AbstractSqsMomProducer;
 import it.pagopa.pn.delivery.PnDeliveryConfigs;
 import it.pagopa.pn.delivery.middleware.NotificationViewedProducer;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Component
-public class SqsNotificationViewedProducer extends AbstractSqsMomProducer<PnDeliveryNotificationViewedEvent> implements NotificationViewedProducer {
+public class SqsNotificationViewedProducer extends AbstractSqsFifoMomProducer<PnDeliveryNotificationViewedEvent> implements NotificationViewedProducer {
 
     public SqsNotificationViewedProducer(SqsClient sqsClient, ObjectMapper objectMapper, PnDeliveryConfigs cfg ) {
         super(sqsClient, cfg.getTopics().getNewNotifications(), objectMapper, PnDeliveryNotificationViewedEvent.class);
