c438ee2d0d053420825202b66d83153a391d4ca0
Alessandro Masci <alessandro.masci@pagopa.it>
2022-10-25 13:39:18 +0200

    PN-2401: add test for coverage

--- src/test/java/it/pagopa/pn/delivery/middleware/notificationdao/NotificationEntityQRDynamoTest.java
+++ src/test/java/it/pagopa/pn/delivery/middleware/notificationdao/NotificationEntityQRDynamoTest.java
@@ -1,31 +1,52 @@
 package it.pagopa.pn.delivery.middleware.notificationdao;
 
-import it.pagopa.pn.commons.exceptions.PnIdConflictException;
-import it.pagopa.pn.delivery.generated.openapi.server.v1.dto.NotificationRecipient;
+import it.pagopa.pn.delivery.PnDeliveryConfigs;
 import it.pagopa.pn.delivery.middleware.notificationdao.entities.NotificationQREntity;
 import it.pagopa.pn.delivery.middleware.notificationdao.entities.RecipientTypeEntity;
 import it.pagopa.pn.delivery.models.InternalNotificationQR;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
 
-import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(MockitoExtension.class)
 class NotificationEntityQRDynamoTest {
+
+    private static DynamoDbTable<NotificationQREntity> qrStorageTable;
 
     private static final String IUN = "UHQX-NMVP-ZKDQ-202210-H-1";
     private static final String RECIPIENT_ID = "PF-aa0c4556-5a6f-45b1-800c-0f4f3c5a57b6";
     private static final String AAR_QR_CODE_VALUE = "VUhRWC1OTVZQLVpLRFEtMjAyMjEwLUgtMV9GUk1UVFI3Nk0wNkI3MTVFXzIyYzJlNDc0LTFmMzgtNGY4Zi04M2FjLWUxOWVlYTFkZTczNg";
+
+    @Mock
+    private DynamoDbEnhancedClient dynamoDbEnhancedClient;
+
+    @Mock
+    private PnDeliveryConfigs.NotificationQRDao notificationQRDao;
+
+    @Mock
+    private PnDeliveryConfigs cfg;
+
     private NotificationQREntityDao qrEntityDao;
 
     @BeforeEach
-    void setup() { qrEntityDao = new NotificationQREntityDaoMock(); }
+    void setup() {
+        notificationQRDao = Mockito.mock(PnDeliveryConfigs.NotificationQRDao.class);
+        qrStorageTable = Mockito.mock(DynamoDbTable.class);
+        dynamoDbEnhancedClient = Mockito.mock( DynamoDbEnhancedClient.class );
+        Mockito.when( cfg.getNotificationQRDao() ).thenReturn( notificationQRDao );
+        Mockito.when( cfg.getNotificationQRDao().getTableName() ).thenReturn( "NotificationsQR" );
+        Mockito.when( dynamoDbEnhancedClient.table( Mockito.anyString(), Mockito.any() )).then( in -> qrStorageTable );
+        qrEntityDao = new NotificationEntityQRDynamo( dynamoDbEnhancedClient, cfg );
+    }
 
     @Test
     void getNotificationByQRSuccess() {
@@ -39,61 +60,13 @@ class NotificationEntityQRDynamoTest {
 
         qrEntityDao.putIfAbsent( entityToInsert );
 
+        Mockito.when( qrStorageTable.getItem( Mockito.any( Key.class ) ) ).thenReturn( entityToInsert );
+
         Optional<InternalNotificationQR> result = qrEntityDao.getNotificationByQR( AAR_QR_CODE_VALUE );
 
         Assertions.assertTrue( result.isPresent() );
         Assertions.assertNotNull( result.get() );
 
-    }
-
-    private static class NotificationQREntityDaoMock implements NotificationQREntityDao {
-
-        private final Map<Key, NotificationQREntity> storage = new ConcurrentHashMap<>();
-
-        @Override
-        public Optional<InternalNotificationQR> getNotificationByQR(String aarQRCode) {
-            Key key = Key.builder()
-                    .partitionValue( aarQRCode )
-                    .build();
-
-            NotificationQREntity notificationQREntity = storage.get( key );
-            if ( Objects.nonNull(notificationQREntity) ) {
-                return Optional.of( InternalNotificationQR.builder()
-                        .iun( notificationQREntity.getIun() )
-                        .aarQRCodeValue( notificationQREntity.getAarQRCodeValue() )
-                        .recipientInternalId( notificationQREntity.getRecipientId() )
-                        .recipientType( NotificationRecipient.RecipientTypeEnum.valueOf( notificationQREntity.getRecipientType().getValue() ))
-                        .build() );
-            } else {
-                return Optional.empty();
-            }
-        }
-
-        @Override
-        public void put(NotificationQREntity notificationQREntity) {
-            Key key = Key.builder()
-                    .partitionValue( notificationQREntity.getAarQRCodeValue() )
-                    .build();
-            storage.put( key, notificationQREntity );
-        }
-
-        @Override
-        public void putIfAbsent(NotificationQREntity notificationQREntity) throws PnIdConflictException {
-            Key key = Key.builder()
-                    .partitionValue( notificationQREntity.getAarQRCodeValue() )
-                    .build();
-            storage.putIfAbsent( key, notificationQREntity );
-        }
-
-        @Override
-        public Optional<NotificationQREntity> get(Key key) {
-            return Optional.empty();
-        }
-
-        @Override
-        public void delete(Key key) {
-
-        }
     }
 
 }
