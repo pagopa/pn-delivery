9ed6342c02bfe1f03dc75a2e37415fdbebcce437
Alessandro Masci <alessandro.masci@pagopa.it>
2022-10-31 18:20:39 +0100

    PN-2511: fix warnings agid tool and add bundle and spectral commands to script

--- scripts/openapi/internal-to-external.sh
+++ scripts/openapi/internal-to-external.sh
@@ -8,3 +8,7 @@ cat docs/openapi/api-internal-web-recipient-v1.yaml \
     | grep -v "# NO EXTERNAL" \
     | sed -e '/# ONLY EXTERNAL/s/^#//' \
     > docs/openapi/api-external-web-recipient-v1.yaml
+
+swagger-cli bundle -o docs/openapi/api-external-b2b-pa-bundle.yaml -t yaml docs/openapi/api-external-b2b-pa-v1.yaml
+
+spectral lint -r https://italia.github.io/api-oas-checker/spectral.yml docs/openapi/api-external-b2b-pa-bundle.yaml
