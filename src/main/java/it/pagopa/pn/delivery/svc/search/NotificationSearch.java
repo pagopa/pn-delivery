b1e33338ac3dabc08a3cf0a94643b699fce40b32
Alessandro Masci <alessandro.masci@pagopa.it>
2022-11-04 12:57:15 +0100

    PN-2534: rm cognitive complexity deanonimize results

--- src/main/java/it/pagopa/pn/delivery/svc/search/NotificationSearch.java
+++ src/main/java/it/pagopa/pn/delivery/svc/search/NotificationSearch.java
@@ -3,10 +3,10 @@ package it.pagopa.pn.delivery.svc.search;
 import it.pagopa.pn.delivery.generated.openapi.clients.datavault.model.BaseRecipientDto;
 import it.pagopa.pn.delivery.generated.openapi.server.v1.dto.NotificationSearchRow;
 import it.pagopa.pn.delivery.middleware.notificationdao.EntityToDtoNotificationMetadataMapper;
-import it.pagopa.pn.delivery.middleware.notificationdao.entities.NotificationMetadataEntity;
 import it.pagopa.pn.delivery.models.ResultPaginationDto;
 import it.pagopa.pn.delivery.pnclient.datavault.PnDataVaultClientImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.CollectionUtils;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -28,27 +28,35 @@ public abstract class NotificationSearch {
 
     protected void deanonimizeResults(ResultPaginationDto<NotificationSearchRow, PnLastEvaluatedKey> globalResult){
         // faccio richiesta a data-vault per restituire i CF non opachi al FE
-        if ( !Collections.emptyList().equals( globalResult.getResultsPage() ) ) {
+        if ( !CollectionUtils.isEmpty( globalResult.getResultsPage() ) ) {
             Set<String> opaqueTaxIds = globalResult.getResultsPage().stream()
                     .map( NotificationSearchRow::getRecipients )
                     .flatMap( Collection::stream )
                     .collect( Collectors.toSet() );
             if (!opaqueTaxIds.isEmpty()) {
-                log.debug( "Opaque tax ids={}", opaqueTaxIds );
-                List<BaseRecipientDto> dataVaultResults = dataVaultClient.getRecipientDenominationByInternalId(new ArrayList<>(opaqueTaxIds));
-                if ( !dataVaultResults.isEmpty() ) {
-                    for (NotificationSearchRow searchRow : globalResult.getResultsPage()) {
-                        List<String> realTaxIds = new ArrayList<>();
-                        for (String internalId : searchRow.getRecipients() ) {
-                            Optional<BaseRecipientDto> match = dataVaultResults.stream().filter(r -> internalId.equals( r.getInternalId() ) ).findFirst();
-                            match.ifPresent(baseRecipientDto -> realTaxIds.add(baseRecipientDto.getTaxId()));
-                        }
-                        searchRow.setRecipients( realTaxIds );
-                    }
-                } else {
-                    log.error( "No result from data-vault for internalIds={}", opaqueTaxIds );
-                }
+                setRecipientsId(globalResult, opaqueTaxIds);
+            } else {
+                log.debug( "Unable to deanonimize results. No opaqueTaxIds in resultsPage" );
             }
+        } else {
+            log.debug( "Unable to deanonimize results. Empty resultsPage" );
+        }
+    }
+
+    private void setRecipientsId(ResultPaginationDto<NotificationSearchRow, PnLastEvaluatedKey> globalResult, Set<String> opaqueTaxIds) {
+        log.debug( "Deanonimize results. Opaque tax ids={}", opaqueTaxIds);
+        List<BaseRecipientDto> dataVaultResults = dataVaultClient.getRecipientDenominationByInternalId(new ArrayList<>(opaqueTaxIds));
+        if ( !dataVaultResults.isEmpty() ) {
+            for (NotificationSearchRow searchRow : globalResult.getResultsPage()) {
+                List<String> realTaxIds = new ArrayList<>();
+                for (String internalId : searchRow.getRecipients() ) {
+                    Optional<BaseRecipientDto> match = dataVaultResults.stream().filter(r -> internalId.equals( r.getInternalId() ) ).findFirst();
+                    match.ifPresent(baseRecipientDto -> realTaxIds.add(baseRecipientDto.getTaxId()));
+                }
+                searchRow.setRecipients( realTaxIds );
+            }
+        } else {
+            log.error( "No result from data-vault for internalIds={}", opaqueTaxIds);
         }
     }
 }
