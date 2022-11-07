48b4efae2428dcc4aa6caf405524ce84227112ce
masciale89 <89906206+masciale89@users.noreply.github.com>
2022-11-07 10:40:02 +0100

    PN-2570: change tool to redocly (#247)

--- scripts/openapi/internal-to-external.sh
+++ scripts/openapi/internal-to-external.sh
@@ -9,6 +9,6 @@ cat docs/openapi/api-internal-web-recipient-v1.yaml \
     | sed -e '/# ONLY EXTERNAL/s/^#//' \
     > docs/openapi/api-external-web-recipient-v1.yaml
 
-swagger-cli bundle -o docs/openapi/api-external-b2b-pa-bundle.yaml -t yaml docs/openapi/api-external-b2b-pa-v1.yaml
+redocly bundle docs/openapi/api-external-b2b-pa-v1.yaml --output docs/openapi/api-external-b2b-pa-bundle.yaml
 
 spectral lint -r https://italia.github.io/api-oas-checker/spectral.yml docs/openapi/api-external-b2b-pa-bundle.yaml
