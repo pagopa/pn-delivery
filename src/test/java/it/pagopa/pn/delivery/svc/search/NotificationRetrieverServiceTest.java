856af7083864c3a98cb6377bc39ec76ae57c887e
Christian Fiorillo <c.fiorillo@mediaticaspa.it>
2022-12-05 14:54:58 +0100

    PN-2773 not found on retrieveNotificationRequestStatus

--- src/test/java/it/pagopa/pn/delivery/svc/search/NotificationRetrieverServiceTest.java
+++ src/test/java/it/pagopa/pn/delivery/svc/search/NotificationRetrieverServiceTest.java
@@ -332,48 +332,6 @@ class NotificationRetrieverServiceTest {
     }
 
     @Test
-    void getNotificationInformationWithSenderIdCheckSuccess() {
-        InternalNotification notification = getNewInternalNotification();
-
-        List<it.pagopa.pn.delivery.generated.openapi.clients.deliverypush.model.TimelineElement> tle = Collections.singletonList( new it.pagopa.pn.delivery.generated.openapi.clients.deliverypush.model.TimelineElement()
-                .elementId( "elementId" )
-                .category( it.pagopa.pn.delivery.generated.openapi.clients.deliverypush.model.TimelineElementCategory.REQUEST_ACCEPTED )
-                .timestamp(  Instant.now()
-                        .atOffset(ZoneOffset.UTC) ));
-        NotificationHistoryResponse timelineStatusHistoryDto = new NotificationHistoryResponse()
-                .timeline( tle )
-                .notificationStatus( it.pagopa.pn.delivery.generated.openapi.clients.deliverypush.model.NotificationStatus.ACCEPTED )
-                .notificationStatusHistory( Collections.singletonList(new it.pagopa.pn.delivery.generated.openapi.clients.deliverypush.model.NotificationStatusHistoryElement()
-                        .status(it.pagopa.pn.delivery.generated.openapi.clients.deliverypush.model.NotificationStatus.ACCEPTED)
-                        .activeFrom( OffsetDateTime.ofInstant( Instant.parse( "2022-06-11T00:00:00.00Z" ), ZoneOffset.UTC ) )) );
-
-        Mockito.when( notificationDao.getNotificationByIun( Mockito.anyString() ) ).thenReturn( Optional.of( notification ) );
-        Mockito.when( pnDeliveryPushClient.getTimelineAndStatusHistory( Mockito.anyString(), Mockito.anyInt(), Mockito.any(OffsetDateTime.class) ) ).thenReturn( timelineStatusHistoryDto );
-
-        ModelMapper mapperNotification = new ModelMapper();
-        mapperNotification.createTypeMap( InternalNotification.class, FullSentNotification.class );
-        Mockito.when( modelMapperFactory.createModelMapper( InternalNotification.class, FullSentNotification.class ) )
-                .thenReturn( mapperNotification );
-
-        InternalNotification result = svc.getNotificationInformationWithSenderIdCheck( IUN, SENDER_ID );
-
-        Assertions.assertNotNull( result );
-        Assertions.assertEquals( IUN, result.getIun() );
-        Assertions.assertEquals( SENDER_ID, result.getSenderPaId() );
-
-    }
-
-    @Test
-    void getNotificationInformationByWrongSenderIdFailure() {
-        InternalNotification notification = getNewInternalNotification();
-
-        Mockito.when( notificationDao.getNotificationByIun( Mockito.anyString() ) ).thenReturn( Optional.of( notification ) );
-
-        Executable todo = () ->  svc.getNotificationInformationWithSenderIdCheck( IUN, WRONG_SENDER_ID );
-        Assertions.assertThrows( PnNotificationNotFoundException.class, todo );
-    }
-
-    @Test
     void getNotificationInfoReturnFirstNoticeCode() {
         InternalNotification notification = getNewInternalNotification();
 
