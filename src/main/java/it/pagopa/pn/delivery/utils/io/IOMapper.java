d1cbc2f3788cfa165521a51980bd8ae623f08a58
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2022-12-22 15:42:32 +0100

    PN-3013 eliminato import non utilizzato

--- src/main/java/it/pagopa/pn/delivery/utils/io/IOMapper.java
+++ src/main/java/it/pagopa/pn/delivery/utils/io/IOMapper.java
@@ -15,7 +15,6 @@ import org.springframework.util.CollectionUtils;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Component
