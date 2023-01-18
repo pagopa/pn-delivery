98d9b4aaaf4d0d48db2951097236fe2137472353
Alessandro Masci <alessandro.masci@pagopa.it>
2023-01-18 11:07:19 +0100

    PN-2188: add controller test for coverage

--- src/test/java/it/pagopa/pn/delivery/rest/PnPaymentEventsControllerTest.java
+++ src/test/java/it/pagopa/pn/delivery/rest/PnPaymentEventsControllerTest.java
@@ -1,6 +1,6 @@
 package it.pagopa.pn.delivery.rest;
 
-import it.pagopa.pn.delivery.PnDeliveryConfigs;
+import it.pagopa.pn.commons.exceptions.PnRuntimeException;
 import it.pagopa.pn.delivery.generated.openapi.server.v1.dto.PaymentEventF24;
 import it.pagopa.pn.delivery.generated.openapi.server.v1.dto.PaymentEventPagoPa;
 import it.pagopa.pn.delivery.generated.openapi.server.v1.dto.PaymentEventsRequestF24;
@@ -18,7 +18,6 @@ import reactor.core.publisher.Mono;
 
 import java.time.OffsetDateTime;
 import java.util.Collections;
-import java.util.List;
 
 @WebFluxTest(PnPaymentEventsController.class)
 class PnPaymentEventsControllerTest {
@@ -64,6 +63,37 @@ class PnPaymentEventsControllerTest {
     }
 
     @Test
+    void paymentEventsPagoPaPostFailure() {
+
+        PaymentEventsRequestPagoPa paymentEventsRequestPagoPa = PaymentEventsRequestPagoPa.builder()
+                .events( Collections.singletonList( PaymentEventPagoPa.builder()
+                        .paymentDate( OffsetDateTime.parse( "2023-01-16T15:30:00Z" ) )
+                        .recipientType( "PF" )
+                        .creditorTaxId( "77777777777" )
+                        .noticeCode( "123456789123456789" )
+                        .build() )
+                )
+                .build();
+
+        Mockito.doThrow( new PnRuntimeException( "test", "description", 400, "errorCode", "element", "detail" ) )
+                .when( paymentEventsService )
+                .handlePaymentEventsPagoPa( Mockito.anyString(), Mockito.anyString(), Mockito.any( PaymentEventsRequestPagoPa.class ) );
+
+        webTestClient.post()
+                .uri("/delivery/events/payment/pagopa")
+                .contentType(MediaType.APPLICATION_JSON)
+                //.accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(paymentEventsRequestPagoPa), PaymentEventsRequestPagoPa.class)
+                .header(PnDeliveryRestConstants.CX_ID_HEADER, CX_ID_PA_ID)
+                .header(PnDeliveryRestConstants.UID_HEADER, "asdasd")
+                .header(PnDeliveryRestConstants.CX_TYPE_HEADER, CX_TYPE_PA)
+                //.header(PnDeliveryRestConstants.CX_GROUPS_HEADER, GROUPS.get(0) +","+GROUPS.get(1) )
+                .exchange()
+                .expectStatus().isBadRequest();
+
+    }
+
+    @Test
     void paymentEventsF24PostSuccess() {
 
         PaymentEventsRequestF24 paymentEventsRequestF24 = PaymentEventsRequestF24.builder()
@@ -90,6 +120,38 @@ class PnPaymentEventsControllerTest {
                 .expectStatus().isNoContent();
 
         Mockito.verify( paymentEventsService ).handlePaymentEventsF24( CX_TYPE_PA, CX_ID_PA_ID, paymentEventsRequestF24 );
+
+    }
+
+    @Test
+    void paymentEventsF24PostFailure() {
+
+        PaymentEventsRequestF24 paymentEventsRequestF24 = PaymentEventsRequestF24.builder()
+                .events( Collections.singletonList( PaymentEventF24.builder()
+                        .paymentDate( OffsetDateTime.parse( "2023-01-16T15:30:00Z" ) )
+                        .iun( "IUN" )
+                        .recipientTaxId( "12345678901" )
+                        .recipientType( "PG" )
+                        .build() )
+                )
+                .build();
+
+        Mockito.doThrow( new PnRuntimeException( "test", "description", 400, "errorCode", "element", "detail" ) )
+                .when( paymentEventsService )
+                .handlePaymentEventsF24( Mockito.anyString(), Mockito.anyString(), Mockito.any( PaymentEventsRequestF24.class ) );
+
+
+        webTestClient.post()
+                .uri("/delivery/events/payment/f24")
+                .contentType(MediaType.APPLICATION_JSON)
+                //.accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(paymentEventsRequestF24), PaymentEventsRequestF24.class)
+                .header(PnDeliveryRestConstants.CX_ID_HEADER, CX_ID_PA_ID)
+                .header(PnDeliveryRestConstants.UID_HEADER, "asdasd")
+                .header(PnDeliveryRestConstants.CX_TYPE_HEADER, CX_TYPE_PA)
+                //.header(PnDeliveryRestConstants.CX_GROUPS_HEADER, GROUPS.get(0) +","+GROUPS.get(1) )
+                .exchange()
+                .expectStatus().isBadRequest();
 
     }
 
