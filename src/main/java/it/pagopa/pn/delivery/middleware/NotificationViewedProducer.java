7100f16293f3ac12fd6d9ad71a7149e7522b1a83
Alessandro Masci <alessandro.masci@pagopa.it>
2023-01-19 11:43:46 +0100

    PN-1731: add producer test for coverage

--- src/main/java/it/pagopa/pn/delivery/middleware/NotificationViewedProducer.java
+++ src/main/java/it/pagopa/pn/delivery/middleware/NotificationViewedProducer.java
@@ -6,15 +6,12 @@ import it.pagopa.pn.api.dto.events.*;
 
 public interface NotificationViewedProducer extends MomProducer<PnDeliveryNotificationViewedEvent> {
 
-    String IMPLEMENTATION_TYPE_PROPERTY_NAME = "pn.middleware.impl.notification-producer";
-
-
     default void sendNotificationViewed( String iun, Instant when, int recipientIndex, NotificationViewDelegateInfo delegateInfo ) {
     	PnDeliveryNotificationViewedEvent event = buildNotificationViewed( iun, when, recipientIndex, delegateInfo );
         this.push( event );
     }
 
-    private PnDeliveryNotificationViewedEvent buildNotificationViewed( String iun, Instant when, int recipientIndex, NotificationViewDelegateInfo delegateInfo ) {
+    default PnDeliveryNotificationViewedEvent buildNotificationViewed( String iun, Instant when, int recipientIndex, NotificationViewDelegateInfo delegateInfo ) {
         String eventId = iun + "_notification_viewed_rec" + recipientIndex;
         return PnDeliveryNotificationViewedEvent.builder()
                 .messageDeduplicationId(eventId)
