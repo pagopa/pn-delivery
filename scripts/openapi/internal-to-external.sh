22ab426c2887536514b760d5206e34b1441c2dd9
Marco Iannaccone <marco.iannaccone@pagopa.it>
2023-03-01 15:24:59 +0100

    PN-4300-4310: v1 removal + added spectral lint with additional checks (commented)

--- scripts/openapi/internal-to-external.sh
+++ scripts/openapi/internal-to-external.sh
@@ -1,22 +1,23 @@
 
-cat docs/openapi/api-internal-b2b-pa-v1.yaml \
+cat docs/openapi/api-internal-b2b-pa.yaml \
     | sed -e '/.*<details no-external>.*/,/<\/details>/ d' \
     | grep -v "# NO EXTERNAL" \
     | sed -e '/# ONLY EXTERNAL/s/^#//' \
-    > docs/openapi/api-external-b2b-pa-v1.yaml
+    > docs/openapi/api-external-b2b-pa.yaml
 
-cat docs/openapi/api-internal-web-recipient-v1.yaml \
+cat docs/openapi/api-internal-web-recipient.yaml \
     | sed -e '/.*<details no-external>.*/,/<\/details>/ d' \
     | grep -v "# NO EXTERNAL" \
     | sed -e '/# ONLY EXTERNAL/s/^#//' \
-    > docs/openapi/api-external-web-recipient-v1.yaml
+    > docs/openapi/api-external-web-recipient.yaml
 
-cat docs/openapi/appio/api-internal-b2b-appio-v1.yaml \
+cat docs/openapi/appio/api-internal-b2b-appio.yaml \
     | sed -e '/.*<details no-external>.*/,/<\/details>/ d' \
     | grep -v "# NO EXTERNAL" \
     | sed -e '/# ONLY EXTERNAL/s/^#//' \
-    > docs/openapi/appio/api-external-b2b-appio-v1.yaml
+    > docs/openapi/appio/api-external-b2b-appio.yaml
 
-redocly bundle docs/openapi/api-external-b2b-pa-v1.yaml --output docs/openapi/api-external-b2b-pa-bundle.yaml
+redocly bundle docs/openapi/api-external-b2b-pa.yaml --output docs/openapi/api-external-b2b-pa-bundle.yaml
 
+#spectral lint -r https://italia.github.io/api-oas-checker/spectral.yml docs/openapi/api-external-b2b-pa-bundle.yaml
 spectral lint -r https://italia.github.io/api-oas-checker/spectral.yml docs/openapi/api-external-b2b-pa-bundle.yaml
