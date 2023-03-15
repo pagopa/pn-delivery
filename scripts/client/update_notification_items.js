de39c7d0f774ef1b61ba8714900844945725c498
Alessandro Masci <alessandro.masci@pagopa.it>
2023-03-15 13:15:57 +0100

    PN-4538: terza versione script aggiornamento pagamento

--- scripts/client/update_notification_items.js
+++ scripts/client/update_notification_items.js
@@ -23,7 +23,7 @@ docClient.scan(params, (err, data) => {
                     if (payment) {
                         const paymentList = transformPayment(payment);
                         console.log("Payment List: ", JSON.stringify(paymentList, null, 2));
-                        const updateExpression = "SET #rec[" + recipientIdx + "].#pay = :paymentList"
+                        const updateExpression = "SET #rec[" + recipientIdx + "].#payList = :paymentList, REMOVE #rec[" + recipientIdx + "].#pay"
                         const updateParams = {
                             TableName: 'Notifications',
                             Key: {
@@ -32,7 +32,8 @@ docClient.scan(params, (err, data) => {
                             UpdateExpression: updateExpression,
                             ExpressionAttributeNames: {
                                 "#rec": 'recipients',
-                                "#pay": 'paymentList'
+                                "#payList": 'paymentList',
+                                "#pay": 'payment'
                             },
                             ExpressionAttributeValues: {
                                 ":paymentList": paymentList
@@ -53,23 +54,12 @@ docClient.scan(params, (err, data) => {
     }
 });
 
-
 function transformPayment(payment) {
     const paymentList = [
       {
         noticeCode: payment.noticeCode,
-        creditorTaxId: payment.creditorTaxId,
-        pagoPaForm: {
-          contentType: payment.pagoPaForm?.contentType,
-          digests: {
-            sha256: payment.pagoPaForm?.digests.sha256,
-          },
-          ref: {
-            key: payment.pagoPaForm?.ref.key,
-            versionToken: payment.pagoPaForm?.ref.versionToken,
-          },
-        },
-      },
+        creditorTaxId: payment.creditorTaxId
+      }
     ];
   
     if (payment.noticeCodeAlternative) {
