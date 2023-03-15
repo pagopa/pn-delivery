3e144cd08c539f1c5a0594f54ed09634699278a3
Alessandro Masci <alessandro.masci@pagopa.it>
2023-03-15 13:38:33 +0100

    PN-4538: script aggiornamento pagamento

--- scripts/client/update_all_notification_with_paymentList.js
+++ scripts/client/update_all_notification_with_paymentList.js
@@ -23,7 +23,7 @@ docClient.scan(params, (err, data) => {
                     if (payment) {
                         const paymentList = transformPayment(payment);
                         console.log("Payment List: ", JSON.stringify(paymentList, null, 2));
-                        const updateExpression = "SET #rec[" + recipientIdx + "].#payList = :paymentList, REMOVE #rec[" + recipientIdx + "].#pay"
+                        const updateExpression = "SET #rec[" + recipientIdx + "].#payList = :paymentList REMOVE #rec[" + recipientIdx + "].#pay"
                         const updateParams = {
                             TableName: 'Notifications',
                             Key: {
@@ -65,20 +65,24 @@ function transformPayment(payment) {
     if (payment.noticeCodeAlternative) {
       paymentList.push({
         noticeCode: payment.noticeCodeAlternative,
-        creditorTaxId: payment.creditorTaxId,
-        pagoPaForm: {
-          contentType: payment.pagoPaForm?.contentType,
-          digests: {
-            sha256: payment.pagoPaForm?.digests.sha256,
-          },
-          ref: {
-            key: payment.pagoPaForm?.ref.key,
-            versionToken: payment.pagoPaForm?.ref.versionToken,
-          },
-        },
+        creditorTaxId: payment.creditorTaxId
       });
     }
   
+    if (payment.pagoPaForm) {
+      return paymentList.map((item) => ({
+          ...item,
+          pagoPaForm: {
+            contentType: payment.pagoPaForm.contentType,
+            digests: {
+              sha256: payment.pagoPaForm.digests.sha256,
+            },
+            ref: {
+              key: payment.pagoPaForm.ref.key,
+              versionToken: payment.pagoPaForm.ref.versionToken,
+            },
+          }
+      }));
+    }
     return paymentList;
   }
-  
