9c1e9db409b30c09bc473edd9b6d1a5d5b548cb1
Alessandro Masci <alessandro.masci@pagopa.it>
2023-03-16 15:55:43 +0100

    PN-4538: fix script aggiungendo paginazione

--- scripts/client/update_all_notification_with_paymentList.js
+++ scripts/client/update_all_notification_with_paymentList.js
@@ -5,88 +5,104 @@ AWS.config.update({region: 'us-east-1', endpoint: 'http://localhost:4566'});
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-TABLE_NAME = 'pn-Notifications'
+TABLE_NAME = 'Notifications'
+SCAN_LIMIT = 1000
 
 const params = {
-    TableName: TABLE_NAME
+  TableName: TABLE_NAME,
+  Limit: SCAN_LIMIT
 };
 
-docClient.scan(params, (err, data) => {
+function scanTable(params, callback) {
+  docClient.scan(params, function(err, data) {
     if (err) {
-        console.error("Error during scan:", JSON.stringify(err, null, 2));
+      callback(err, null);
     } else {
-        //console.log("Scan succeeded:", JSON.stringify(data, null, 2));
-        data.Items.forEach(function (item) {
-            const key = item.iun;
-            console.log("Key: ", key);
-            if (item.recipients) {
-                var recipientIdx = 0;
-                item.recipients.forEach(function (recipient) {
-                    const payment = recipient.payment;
-                    console.log("Payment: ", payment);
-                    if (payment) {
-                        const paymentList = transformPayment(payment);
-                        console.log("Payment List: ", JSON.stringify(paymentList, null, 2));
-                        const updateExpression = "SET #rec[" + recipientIdx + "].#payList = :paymentList REMOVE #rec[" + recipientIdx + "].#pay"
-                        const updateParams = {
-                            TableName: TABLE_NAME,
-                            Key: {
-                                "iun": key
-                            },
-                            UpdateExpression: updateExpression,
-                            ExpressionAttributeNames: {
-                                "#rec": 'recipients',
-                                "#payList": 'paymentList',
-                                "#pay": 'payment'
-                            },
-                            ExpressionAttributeValues: {
-                                ":paymentList": paymentList
-                            }
-                        }
-                        docClient.update(updateParams, (err, data) => {
-                            if (err) {
-                                console.error("Errore nell'aggiornamento dell'elemento:", JSON.stringify(err, null, 2));
-                            } else {
-                                console.log("Elemento aggiornato con successo:", JSON.stringify(data, null, 2));
-                            }
-                        })
-                    }
-                    recipientIdx++;
-                });     
-            }
-        });
+      callback(null, data);
+      
+      if (typeof data.LastEvaluatedKey !== 'undefined') {
+        params.ExclusiveStartKey = data.LastEvaluatedKey;
+        scanTable(params, callback);
+      }
     }
-});
+  });
+}
+
+scanTable(params, function(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    data.Items.forEach(function (item) {
+      const key = item.iun;
+      console.log("Key: ", key);
+      if (item.recipients) {
+        var recipientIdx = 0;
+        item.recipients.forEach(function (recipient) {
+          const payment = recipient.payment;
+          console.log("Payment: ", payment);
+          if (payment) {
+            const paymentList = transformPayment(payment);
+            console.log("Payment List: ", JSON.stringify(paymentList, null, 2));
+            const updateExpression = "SET #rec[" + recipientIdx + "].#payList = :paymentList REMOVE #rec[" + recipientIdx + "].#pay"
+            const updateParams = {
+              TableName: TABLE_NAME,
+              Key: {
+                "iun": key
+              },
+              UpdateExpression: updateExpression,
+              ExpressionAttributeNames: {
+                "#rec": 'recipients',
+                "#payList": 'paymentList',
+                "#pay": 'payment'
+              },
+              ExpressionAttributeValues: {
+                ":paymentList": paymentList
+              }
+            }
+            docClient.update(updateParams, (err, data) => {
+              if (err) {
+                console.error("Errore nell'aggiornamento dell'elemento:", JSON.stringify(err, null, 2));
+              } else {
+                console.log("Elemento aggiornato con successo:", JSON.stringify(data, null, 2));
+              }
+            })
+          }
+          recipientIdx++;
+        });     
+      }
+    });
+  }
+})
 
 function transformPayment(payment) {
-    const paymentList = [
-      {
-        noticeCode: payment.noticeCode,
-        creditorTaxId: payment.creditorTaxId
-      }
-    ];
-  
-    if (payment.noticeCodeAlternative) {
-      paymentList.push({
-        noticeCode: payment.noticeCodeAlternative,
-        creditorTaxId: payment.creditorTaxId
-      });
+  const paymentList = [
+    {
+      noticeCode: payment.noticeCode,
+      creditorTaxId: payment.creditorTaxId
     }
+  ];
   
-    if (payment.pagoPaForm) {
-      return paymentList.map((item) => ({
-          ...item,
-          pagoPaForm: {
-            contentType: payment.pagoPaForm.contentType,
-            digests: {
-              sha256: payment.pagoPaForm.digests.sha256,
-            },
-            ref: {
-              key: payment.pagoPaForm.ref.key,
-              versionToken: payment.pagoPaForm.ref.versionToken,
-            },
-          }
-      }));
-    }
-    return paymentList;
+  if (payment.noticeCodeAlternative) {
+    paymentList.push({
+      noticeCode: payment.noticeCodeAlternative,
+      creditorTaxId: payment.creditorTaxId
+    });
   }
+  
+  if (payment.pagoPaForm) {
+    return paymentList.map((item) => ({
+      ...item,
+      pagoPaForm: {
+        contentType: payment.pagoPaForm.contentType,
+        digests: {
+          sha256: payment.pagoPaForm.digests.sha256,
+        },
+        ref: {
+          key: payment.pagoPaForm.ref.key,
+          versionToken: payment.pagoPaForm.ref.versionToken,
+        },
+      }
+    }));
+  }
+  return paymentList;
+}
