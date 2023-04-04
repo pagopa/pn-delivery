7deb4fc40db4f0f2329afad46b0550618a22c7ea
Alessandro Masci <alessandro.masci@pagopa.it>
2023-04-04 18:39:55 +0200

    PN-5123: resolve code smell

--- src/main/java/it/pagopa/pn/delivery/middleware/notificationdao/EntityToDtoNotificationMapper.java
+++ src/main/java/it/pagopa/pn/delivery/middleware/notificationdao/EntityToDtoNotificationMapper.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Component;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static it.pagopa.pn.delivery.exception.PnDeliveryExceptionCodes.ERROR_CODE_DELIVERY_UNSUPPORTED_PHYSICALCOMMUNICATIONTYPE;
 
