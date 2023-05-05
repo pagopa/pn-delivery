f268ca076e99452eb50b785e672fcba557f4393c
Fabrizio.D'Ammassa <fabrizio.dammassa@pagopa.it>
2023-05-05 11:17:29 +0200

    Created node project and README for multipayment notifications refactor script

--- scripts/client/multipayment_notification/update_all_notification_with_version.js
+++ scripts/client/multipayment_notification/update_all_notification_with_version.js
@@ -3,14 +3,32 @@ const AWS = require('aws-sdk');
 AWS.config.credentials = credentials;
 AWS.config.update({region: 'us-east-1', endpoint: 'http://localhost:4566'});*/
 
-// per lanciarlo in ambiente dev
-var credentials = new AWS.SharedIniFileCredentials({profile: 'dev'});
-AWS.config.credentials = credentials;
-AWS.config.update({region: 'eu-west-3'});
+const arguments = process.argv ;
+  
+if(arguments.length<=2){
+  console.error("Specify AWS profile as argument")
+  process.exit(1)
+}
+
+const awsProfile = awsProfile
+
+console.log("Using profile "+awsProfile)
+
+let credentials = null
+
+process.env.AWS_SDK_LOAD_CONFIG=1
+if(credentials.indexOf('sso_')>=0){ // sso profile
+  credentials = new AWS.SsoCredentials({profile:awsProfile});
+  AWS.config.credentials = credentials;
+} else { // IAM profile
+  credentials = new AWS.SharedIniFileCredentials({profile: awsProfile});
+  AWS.config.credentials = credentials;
+}
+AWS.config.update({region: 'eu-south-1'});
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-TABLE_NAME = 'pn-Notifications-2804'
+TABLE_NAME = 'pn-Notifications'
 SCAN_LIMIT = 2000 // max 2000
 DELAY_MS = 1000; //1 second
 
