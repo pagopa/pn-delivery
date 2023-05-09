4e31c7a45069b9bbdffa752062200786ccc069c1
masciale89 <89906206+masciale89@users.noreply.github.com>
2023-05-09 18:03:29 +0200

    feature/PN-5740 (#465)
    * PN-5740: rm other recipient for IO detail notification request
    
    * PN-5740: trigger build

--- src/main/java/it/pagopa/pn/delivery/utils/io/IOMapper.java
+++ src/main/java/it/pagopa/pn/delivery/utils/io/IOMapper.java
@@ -1,6 +1,7 @@
 package it.pagopa.pn.delivery.utils.io;
 
 import it.pagopa.pn.delivery.generated.openapi.appio.v1.dto.IOReceivedNotification;
+import it.pagopa.pn.delivery.generated.openapi.appio.v1.dto.NotificationRecipient;
 import it.pagopa.pn.delivery.generated.openapi.appio.v1.dto.ThirdPartyAttachment;
 import it.pagopa.pn.delivery.generated.openapi.appio.v1.dto.ThirdPartyMessage;
 import it.pagopa.pn.delivery.generated.openapi.server.v1.dto.NotificationDocument;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import java.util.Collections;
 import java.util.List;
@@ -38,7 +40,16 @@ public class IOMapper {
     public IOReceivedNotification mapToDetails(InternalNotification internalNotification) {
         if(internalNotification == null) return null;
 
-        return modelMapper.map( internalNotification, IOReceivedNotification.class );
+        IOReceivedNotification ioReceivedNotification = modelMapper.map(internalNotification, IOReceivedNotification.class);
+        List<NotificationRecipient> filteredNotificationRecipients = ioReceivedNotification.getRecipients()
+                .stream()
+                .filter(rec -> StringUtils.hasText(rec.getTaxId()))
+                .toList();
+        if(!CollectionUtils.isEmpty( filteredNotificationRecipients )) {
+            ioReceivedNotification.setRecipients( filteredNotificationRecipients );
+        }
+
+        return ioReceivedNotification;
     }
 
     public List<ThirdPartyAttachment> mapToThirdPartyAttachment(List<NotificationDocument> documents, String iun) {
