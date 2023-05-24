1ce074888d6962d0302b89cce7ba9ac4785ff6db
Alessandro Masci <alessandro.masci@pagopa.it>
2023-05-24 18:23:32 +0200

    PN-5514: add lollipopActivation

---
+++ src/main/java/it/pagopa/pn/delivery/springbootcfg/LollipopActivation.java
@@ -0,0 +1,8 @@
+package it.pagopa.pn.delivery.springbootcfg;
+
+import it.pagopa.pn.commons.configs.lollipop.PNHttpVerifierConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LollipopActivation extends PNHttpVerifierConfiguration {
+}
