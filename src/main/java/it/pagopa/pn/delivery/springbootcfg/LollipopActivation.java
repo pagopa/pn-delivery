1292a8b09897865eb2bcc960cdcb6dd1bdd7cc7e
Alessandro Masci <alessandro.masci@pagopa.it>
2023-05-26 16:23:21 +0200

    PN-5514: add lollipop activation flag

--- src/main/java/it/pagopa/pn/delivery/springbootcfg/LollipopActivation.java
+++ src/main/java/it/pagopa/pn/delivery/springbootcfg/LollipopActivation.java
@@ -1,8 +1,10 @@
 package it.pagopa.pn.delivery.springbootcfg;
 
 import it.pagopa.pn.commons.configs.lollipop.PNHttpVerifierConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty(prefix="lollipop", name="active", havingValue = "true")
 public class LollipopActivation extends PNHttpVerifierConfiguration {
 }
