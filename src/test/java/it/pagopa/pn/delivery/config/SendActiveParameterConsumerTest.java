852b02812ff1c3aa9cc86ae5e6cae7f32de5b614
masciale89 <89906206+masciale89@users.noreply.github.com>
2023-06-19 10:12:43 +0200

    feature/PN-6736 (#503)
    * PN-6736: add send activation feature flag
    
    * PN-6736: add test for coverage

---
+++ src/test/java/it/pagopa/pn/delivery/config/SendActiveParameterConsumerTest.java
@@ -0,0 +1,40 @@
+package it.pagopa.pn.delivery.config;
+
+import it.pagopa.pn.commons.abstractions.ParameterConsumer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Optional;
+@TestPropertySource(properties = { "pn.delivery.features.is-send-active-default-value=true"})
+class SendActiveParameterConsumerTest {
+
+    public static final String PA_TAX_ID_ACTIVE = "80016350821";
+    public static final String PA_TAX_ID_INACTIVE = "01234567891";
+
+    private ParameterConsumer parameterConsumer;
+
+    private SendActiveParameterConsumer sendActiveParameterConsumer;
+
+    @BeforeEach
+    void setup() {
+        this.parameterConsumer = Mockito.mock( ParameterConsumer.class );
+        this.sendActiveParameterConsumer = new SendActiveParameterConsumer(parameterConsumer);
+    }
+
+    @Test
+    void isSendActive() {
+        SendActiveParameterConsumer.PaTaxIdIsSendActive[] paTaxIdIsSendActives = new SendActiveParameterConsumer.PaTaxIdIsSendActive[2];
+        paTaxIdIsSendActives[0] = new SendActiveParameterConsumer.PaTaxIdIsSendActive(PA_TAX_ID_ACTIVE, true );
+        paTaxIdIsSendActives[1] = new SendActiveParameterConsumer.PaTaxIdIsSendActive( PA_TAX_ID_INACTIVE, false );
+
+        Mockito.when( parameterConsumer.getParameterValue( Mockito.anyString(), Mockito.any() ) )
+                .thenReturn( Optional.of(paTaxIdIsSendActives) );
+
+        Assertions.assertTrue( sendActiveParameterConsumer.isSendActive( PA_TAX_ID_ACTIVE ) );
+        Assertions.assertFalse( sendActiveParameterConsumer.isSendActive( PA_TAX_ID_INACTIVE ) );
+
+    }
+}
