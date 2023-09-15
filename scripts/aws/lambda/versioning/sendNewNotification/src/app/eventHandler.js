ecf3f28f4b46fffad4eb77a89e73cac2ac0ba5e3
Alessandro Masci <alessandro.masci@pagopa.it>
2023-09-15 10:10:00 +0200

    add validateNewNotification

--- scripts/aws/lambda/versioning/sendNewNotification/src/app/eventHandler.js
+++ scripts/aws/lambda/versioning/sendNewNotification/src/app/eventHandler.js
@@ -8,5 +8,18 @@ exports.handleEvent = async (event) => {
         return generateResponse({ resultCode: '404.00', resultDescription: 'Not found', errorList: isRequestValid }, 404, {})
     }
 
+    const eventValidationErrors = validateNewNotification(event.body)
+    if(eventValidationErrors.length>0){
+        return generateResponse({ resultCode: '400.00', resultDescription: 'Validation error', errorList: eventValidationErrors }, 400, {})
+    }
+
+    // creazione oggetto NewNotificationRequestV21
+
+    // mapping tra NewNotificationRequest e NewNotificationRequestV21
+
+    // post verso pn-delivery
+
+    // creazione della response
+
 
     };
