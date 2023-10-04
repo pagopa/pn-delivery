b1ff8f8910acf6df04bff3e973b4493d5e974818
EmanueleBonofiglio <emanuele.bonofiglio@grupposcai.it>
2023-10-05 00:36:57 +0200

    PN-8065 invocazione getRootSenderId

--- src/main/java/it/pagopa/pn/delivery/pnclient/externalregistries/PnExternalRegistriesClientImpl.java
+++ src/main/java/it/pagopa/pn/delivery/pnclient/externalregistries/PnExternalRegistriesClientImpl.java
@@ -3,11 +3,14 @@ package it.pagopa.pn.delivery.pnclient.externalregistries;
 import it.pagopa.pn.commons.log.PnLogger;
 import it.pagopa.pn.delivery.generated.openapi.msclient.externalregistries.v1.api.InternalOnlyApi;
 import it.pagopa.pn.delivery.generated.openapi.msclient.externalregistries.v1.api.PaymentInfoApi;
+import it.pagopa.pn.delivery.generated.openapi.msclient.externalregistries.v1.api.RootSenderIdApi;
 import it.pagopa.pn.delivery.generated.openapi.msclient.externalregistries.v1.model.PaGroup;
 import it.pagopa.pn.delivery.generated.openapi.msclient.externalregistries.v1.model.PaGroupStatus;
 import it.pagopa.pn.delivery.generated.openapi.msclient.externalregistries.v1.model.PaymentInfo;
+import it.pagopa.pn.delivery.generated.openapi.msclient.externalregistries.v1.model.RootSenderIdResponse;
 import lombok.CustomLog;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
@@ -20,7 +23,7 @@ public class PnExternalRegistriesClientImpl {
 
     private final PaymentInfoApi paymentInfoApi;
     private final InternalOnlyApi internalOnlyApi;
-
+    private final RootSenderIdApi rootSenderIdApi;
 
     public PaymentInfo getPaymentInfo(String paTaxId, String noticeNumber ) {
         log.logInvokingExternalService(PnLogger.EXTERNAL_SERVICES.PN_EXTERNAL_REGISTRIES, "getPaymentInfo");
@@ -34,6 +37,17 @@ public class PnExternalRegistriesClientImpl {
         } catch (Exception exc) {
             log.error("Error during retrieve of the groups", exc);
             return Collections.emptyList();
+        }
+    }
+
+    @Cacheable("aooSenderIdCache")
+    public String getRootSenderId(String senderId){
+        try{
+            RootSenderIdResponse rootSenderIdPrivate = rootSenderIdApi.getRootSenderIdPrivate(senderId);
+            return rootSenderIdPrivate.getRootId();
+        }catch (Exception exc) {
+            log.error("Error during map rootSenderID", exc);
+            return "";
         }
     }
 }
