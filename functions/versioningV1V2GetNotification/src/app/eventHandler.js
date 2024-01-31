d782bbf58a5077fbc6953ae230cf68f41b9b2002
fabriziolana <fabrizio.lana@necsy.it>
2024-01-31 17:49:30 +0100

    PN-9708: modifiche api v23

--- functions/versioningV1V2GetNotification/src/app/eventHandler.js
+++ functions/versioningV1V2GetNotification/src/app/eventHandler.js
@@ -289,16 +289,28 @@ exports.versioning = async (event, context) => {
     {
       if (tl.category == "SEND_ANALOG_PROGRESS")
       {
-        console.log("transformTimeline - SEND_ANALOG_PROGRESS", tl);
+        console.log("transformTimeline - " + tl.category);
         // in 2.3 è stato aggiunto nel detail la property serviceLevel, che va rimossa
         delete tl.details.serviceLevel;
       }
-      else if (tl.category == "NOTIFICATION_VIEWED")
+      else if (tl.category == "NOTIFICATION_VIEWED"
+                || tl.category == "REFINEMENT"
+                || tl.category == "SEND_DIGITAL_PROGRESS"
+                || tl.category == "PAYMENT")
       {
-        console.log("transformTimeline - NOTIFICATION_VIEWED", tl);
+        console.log("transformTimeline - " + tl.category);
         // in 2.3 è stato aggiunto nel detail la property eventTimestamp, che va rimossa
         delete tl.details.eventTimestamp;
       }
+      else if (tl.category == "SCHEDULE_ANALOG_WORKFLOW"
+                      || tl.category == "SCHEDULE_DIGITAL_WORKFLOW")
+      {
+        console.log("transformTimeline - " + tl.category);
+        // in 2.3 è stato aggiunto nel detail la property schedulingDate, che va rimossa
+        delete tl.details.schedulingDate;
+      }
+
+
     }
 
     return tl;
