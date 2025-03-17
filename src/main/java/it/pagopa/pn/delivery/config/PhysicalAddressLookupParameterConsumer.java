413a54ea04c241a8591bbabd56bcb3f4af74d66f
empassaro <emanuele.passaro@emeal.nttdata.com>
2025-03-17 16:17:55 +0100

    [PN-14266] feat: Added feature flag for physicalAddressLookup and parameter store consumer

---
+++ src/main/java/it/pagopa/pn/delivery/config/PhysicalAddressLookupParameterConsumer.java
@@ -0,0 +1,37 @@
+package it.pagopa.pn.delivery.config;
+
+import it.pagopa.pn.commons.abstractions.ParameterConsumer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Configuration
+public class PhysicalAddressLookupParameterConsumer {
+
+    private final ParameterConsumer parameterConsumer;
+
+    private static final String PARAMETER_NAME = "PAsActiveForPhysicalAddressLookup";
+
+    public PhysicalAddressLookupParameterConsumer(ParameterConsumer parameterConsumer) {
+        this.parameterConsumer = parameterConsumer;
+    }
+
+    public List<String> getActivePAsForPhysicalAddressLookup() {
+        log.debug("Start getActivePAsForPhysicalAddressLookup");
+
+        Optional<String[]> optionalActivePasForPhysicalAddressLookup = parameterConsumer.getParameterValue(
+                PARAMETER_NAME, String[].class);
+        if( optionalActivePasForPhysicalAddressLookup.isPresent() ) {
+            log.info("Active PA for physical address lookup found, paIds={}", Arrays.toString(optionalActivePasForPhysicalAddressLookup.get()));
+            return Arrays.stream(optionalActivePasForPhysicalAddressLookup.get()).toList();
+        }
+
+        log.debug("There are not active PAs for physicalAddressLookup");
+        return Collections.emptyList();
+    }
+}
