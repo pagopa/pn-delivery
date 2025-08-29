1613fd9741a7f36b0ef03b899d3e26e778827719
empassaro <emanuele.passaro@emeal.nttdata.com>
2025-08-29 12:24:55 +0200

    fix: removed handling of tos error from qr code api response

--- src/main/java/it/pagopa/pn/delivery/springbootcfg/PnResponseEntityExceptionHandlerActivation.java
+++ src/main/java/it/pagopa/pn/delivery/springbootcfg/PnResponseEntityExceptionHandlerActivation.java
@@ -4,8 +4,6 @@ package it.pagopa.pn.delivery.springbootcfg;
 import it.pagopa.pn.commons.exceptions.ExceptionHelper;
 import it.pagopa.pn.commons.exceptions.PnResponseEntityExceptionHandler;
 import it.pagopa.pn.delivery.exception.PnIoMandateNotFoundException;
-import it.pagopa.pn.delivery.generated.openapi.server.appio.v1.dto.CheckQrForbiddenResponse;
-import it.pagopa.pn.delivery.generated.openapi.server.appio.v1.dto.CheckQrForbiddenResponseDetail;
 import it.pagopa.pn.delivery.generated.openapi.server.appio.v1.dto.ResponseCheckQrMandateDto;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.ResponseEntity;
@@ -19,20 +17,7 @@ public class PnResponseEntityExceptionHandlerActivation extends PnResponseEntity
     }
 
     @ExceptionHandler({PnIoMandateNotFoundException.class})
-    public ResponseEntity<CheckQrForbiddenResponse> handleIoMandateNotFoundException(PnIoMandateNotFoundException ex) {
-        CheckQrForbiddenResponse checkQrForbiddenResponse = buildCheckQrForbiddenResponse(ex.getResponseCheckQrMandateDto());
-        return ResponseEntity.status(ex.getStatus()).body(checkQrForbiddenResponse);
+    public ResponseEntity<ResponseCheckQrMandateDto> handleIoMandateNotFoundException(PnIoMandateNotFoundException ex) {
+        return ResponseEntity.status(ex.getStatus()).body(ex.getResponseCheckQrMandateDto());
     }
-
-    private CheckQrForbiddenResponse buildCheckQrForbiddenResponse(ResponseCheckQrMandateDto responseCheckQrMandateDto) {
-        return CheckQrForbiddenResponse.builder()
-                .errorType(CheckQrForbiddenResponse.ErrorTypeEnum.MANDATE_NOT_PRESENT)
-                .detail(CheckQrForbiddenResponseDetail.builder()
-                        .iun(responseCheckQrMandateDto.getIun())
-                        .recipientInfo(responseCheckQrMandateDto.getRecipientInfo())
-                        .build())
-                .build();
-    }
-
-
 }
--- src/test/java/it/pagopa/pn/delivery/springbootcfg/PnResponseEntityExceptionHandlerActivationTest.java
+++ src/test/java/it/pagopa/pn/delivery/springbootcfg/PnResponseEntityExceptionHandlerActivationTest.java
@@ -2,7 +2,6 @@ package it.pagopa.pn.delivery.springbootcfg;
 
 import it.pagopa.pn.commons.exceptions.ExceptionHelper;
 import it.pagopa.pn.delivery.exception.PnIoMandateNotFoundException;
-import it.pagopa.pn.delivery.generated.openapi.server.appio.v1.dto.CheckQrForbiddenResponse;
 import it.pagopa.pn.delivery.generated.openapi.server.appio.v1.dto.ResponseCheckQrMandateDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,11 +25,10 @@ class PnResponseEntityExceptionHandlerActivationTest {
     @Test
     void handleIoMandateNotFoundException() {
         PnIoMandateNotFoundException exception = new PnIoMandateNotFoundException(new ResponseCheckQrMandateDto());
-        ResponseEntity<CheckQrForbiddenResponse> response = handler.handleIoMandateNotFoundException(exception);
+        ResponseEntity<ResponseCheckQrMandateDto> response = handler.handleIoMandateNotFoundException(exception);
         assertNotNull(response);
         assertEquals(403, response.getStatusCodeValue());
         assertNotNull(response.getBody());
-        assertEquals(CheckQrForbiddenResponse.ErrorTypeEnum.MANDATE_NOT_PRESENT, response.getBody().getErrorType());
     }
 
 }
