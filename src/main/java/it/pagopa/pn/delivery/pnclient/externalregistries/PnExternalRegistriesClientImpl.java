ec8593aef4912cb70315ff2dff06c0ed79747c12
ECARADM5R <matteo.caradonna@emeal.nttdata.com>
2026-03-24 15:36:03 +0100

    [PN-19129] - copilot review

--- src/main/java/it/pagopa/pn/delivery/pnclient/externalregistries/PnExternalRegistriesClientImpl.java
+++ src/main/java/it/pagopa/pn/delivery/pnclient/externalregistries/PnExternalRegistriesClientImpl.java
@@ -25,15 +25,15 @@ public class PnExternalRegistriesClientImpl {
     private final RootSenderIdApi rootSenderIdApi;
     private final InfoPaApi infoPaApi;
 
-    public PaymentInfo getPaymentInfo(String paTaxId, String noticeNumber ) {
+    public PaymentInfo getPaymentInfo(String paTaxId, String noticeNumber) {
         log.logInvokingExternalService(PnLogger.EXTERNAL_SERVICES.PN_EXTERNAL_REGISTRIES, "getPaymentInfo");
-        return paymentInfoApi.getPaymentInfo( paTaxId, noticeNumber );
+        return paymentInfoApi.getPaymentInfo(paTaxId, noticeNumber);
     }
 
     public List<PaGroup> getGroups(String senderId, boolean onlyActive) {
         try {
             log.logInvokingExternalService(PnLogger.EXTERNAL_SERVICES.PN_EXTERNAL_REGISTRIES, "getGroups");
-            return internalOnlyApi.getAllGroupsPrivate(senderId, onlyActive? PaGroupStatus.ACTIVE:null);
+            return internalOnlyApi.getAllGroupsPrivate(senderId, onlyActive ? PaGroupStatus.ACTIVE : null);
         } catch (Exception exc) {
             log.error("Error during retrieve of the groups", exc);
             return Collections.emptyList();
@@ -41,19 +41,19 @@ public class PnExternalRegistriesClientImpl {
     }
 
     @Cacheable(value = "aooSenderIdCache", cacheManager = "pnCacheManager")
-    public String getRootSenderId(String senderId){
-        try{
+    public String getRootSenderId(String senderId) {
+        try {
             RootSenderIdResponse rootSenderIdPrivate = rootSenderIdApi.getRootSenderIdPrivate(senderId);
             return rootSenderIdPrivate.getRootId();
-        }catch (Exception exc) {
+        } catch (Exception exc) {
             String message = String.format("Error during map rootSenderID = %s [exception received = %s]", senderId, exc);
             log.error(message);
             throw new PnRootIdNonFountException(message);
         }
     }
 
-    public PaInfo getOnePa(String paId){
-            log.logInvokingExternalService(PnLogger.EXTERNAL_SERVICES.PN_EXTERNAL_REGISTRIES, "getOnePa");
-            return infoPaApi.getOnePa(paId);
+    public PaInfo getOnePa(String paId) {
+        log.logInvokingExternalService(PnLogger.EXTERNAL_SERVICES.PN_EXTERNAL_REGISTRIES, "getOnePa");
+        return infoPaApi.getOnePa(paId);
     }
 }
