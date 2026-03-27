c9178a29fd28ddf1ab02951f43b170c57e9f6af0
ECARADM5R <matteo.caradonna@emeal.nttdata.com>
2026-03-27 15:22:10 +0100

    PN-19130 - Merge typo

--- src/main/java/it/pagopa/pn/delivery/PnDeliveryConfigs.java
+++ src/main/java/it/pagopa/pn/delivery/PnDeliveryConfigs.java
@@ -102,11 +102,9 @@ public class PnDeliveryConfigs {
 
     private String documentNumberOfPagesTagKey;
 
-<<<<<<< feature/PN-19130
     private boolean enableSenderTaxIdCongruence;
-=======
+
     private Duration infoPaCacheDuration;
->>>>>>> feature/PN-19128-epic-branch
 
     @PostConstruct
     public void init(){
