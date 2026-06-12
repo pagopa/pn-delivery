5f14a25d0d6bbdbda36620ba1d5344bf07bb5734
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2026-06-12 16:44:21 +0200

    [PN-20219] feat: Definizione API e modelli interni di dettaglio notifica differenziati per dominio

---
+++ src/main/java/it/pagopa/pn/delivery/models/NotificationDetail.java
@@ -0,0 +1,6 @@
+package it.pagopa.pn.delivery.models;
+
+public interface NotificationDetail {
+    InternalNotification getNotification();
+    void setNotification(InternalNotification notification);
+}
