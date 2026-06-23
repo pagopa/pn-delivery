c2a9acfa77e304e0ca0dd8eee2e3c2d3a99bde97
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2026-06-23 12:16:55 +0200

    [PN-20222] feat: upgraded version of status enum

--- src/main/java/it/pagopa/pn/delivery/rest/PnSentNotificationsController.java
+++ src/main/java/it/pagopa/pn/delivery/rest/PnSentNotificationsController.java
@@ -307,7 +307,7 @@ public class PnSentNotificationsController implements SenderReadB2BApi, SenderRe
                 .iun(iun)
                 .build();
         logEvent.log();
-        if ( InformalNotificationStatusV1.IN_VALIDATION.equals( informalNotificationDetail.getNotificationStatus() )
+        if ( InformalNotificationStatusV1.ACCEPTED.equals( informalNotificationDetail.getNotificationStatus() )
                 || InformalNotificationStatusV1.REFUSED.equals( informalNotificationDetail.getNotificationStatus() ) ) {
             logEvent.generateFailure("Unable to find informal notification with iun={} cause status={}", internalNotification.getIun(), informalNotificationDetail.getNotificationStatus()).log();
             throw new PnNotificationNotFoundException( "Unable to find informal notification with iun="+ internalNotification.getIun() );
