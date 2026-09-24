6c0d05af6a74f370b6bc4c7604c3020b32c54f20
empassaro <emanuele.passaro@emeal.nttdata.com>
2026-09-24 18:36:22 +0200

    fix: added CampaignServiceCachedProvider as mock in MockAWSObjectsTest

--- src/test/java/it/pagopa/pn/delivery/MockAWSObjectsTest.java
+++ src/test/java/it/pagopa/pn/delivery/MockAWSObjectsTest.java
@@ -1,6 +1,7 @@
 package it.pagopa.pn.delivery;
 
 import io.awspring.cloud.autoconfigure.messaging.SqsAutoConfiguration;
+import it.pagopa.pn.commons.db.campaign.CampaignServiceCachedProvider;
 import it.pagopa.pn.delivery.config.InformalNotificationSendPaParameterConsumer;
 import it.pagopa.pn.delivery.middleware.AsseverationEventsProducer;
 import it.pagopa.pn.delivery.middleware.notificationviewedproducer.strategy.producer.NotificationViewedProducer;
@@ -22,4 +23,7 @@ public abstract class MockAWSObjectsTest {
 
     @MockBean
     private InformalNotificationSendPaParameterConsumer informalNotificationSendPaParameterConsumer;
+
+    @MockBean
+    private CampaignServiceCachedProvider campaignServiceCachedProvider;
 }
